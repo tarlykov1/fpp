ca55fd4e4e917da2dc7483b3a7808af1d3bc822b
tarlykov1 <44412162+tarlykov1@users.noreply.github.com>
2026-05-25 22:37:24 +0300

    Remove GeneratePress footer credit in child theme

--- functions.php
+++ functions.php
@@ -23,3 +23,18 @@ function generatepress_child_enqueue_styles() {
 		wp_get_theme()->get( 'Version' )
 	);
 }
+
+/**
+ * Remove the default "Built with GeneratePress" footer credit.
+ *
+ * @param string $copyright Footer copyright HTML.
+ * @return string
+ */
+function generatepress_child_remove_generatepress_credit( $copyright ) {
+	return preg_replace(
+		'#\s*(?:•|&middot;|&#183;)\s*Built with\s*<a[^>]*generatepress\.com[^>]*>.*?</a>#iu',
+		'',
+		$copyright
+	);
+}
+add_filter( 'generate_copyright', 'generatepress_child_remove_generatepress_credit' );
